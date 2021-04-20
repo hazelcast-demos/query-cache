e490865fb21cd56ef26eeec608880472c4c34392
Nicolas Frankel <nicolas@frankel.ch>
2021-04-20 11:33:38 +0200

    Add a Spring Shell command to measure the time of a query execution that involves sort

--- client/src/main/java/org/hazelcast/query/client/TimeCommand.java
+++ client/src/main/java/org/hazelcast/query/client/TimeCommand.java
@@ -33,4 +33,27 @@ public class TimeCommand {
         var formatter = NumberFormat.getInstance();
         return formatter.format(end - start) + " ns";
     }
+
+    @ShellMethod(
+            key = {"time-component-range", "time-comp-range", "tcr"},
+            value = "Measure the time it takes to query the Hazelcast analytics map by component's name and below a instant"
+    )
+    public String timeByComponentNameAndWithinTimeRange(
+            @ShellOption(
+                    help = "The name of the component. For the demo, valid values are first-name, last-name, range, success-button and first-button"
+            ) String name,
+            @ShellOption(
+                    help = "The instant max value. For the demo, instant ranges from 0 to 99,999"
+            ) long instant
+
+    ) {
+        var map = client.getMap("analytics");
+        var entry = Predicates.newPredicateBuilder().getEntryObject();
+        var predicate = entry.get("component").equal(name).and(entry.get("instant").lessThan(instant));
+        var start = System.nanoTime();
+        map.values(predicate);
+        var end = System.nanoTime();
+        var formatter = NumberFormat.getInstance();
+        return formatter.format(end - start) + " ns";
+    }
 }
